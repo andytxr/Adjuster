345061a547c1721c1d7db015e85687e31f699a4f
andytxr <62211442+andytxr@users.noreply.github.com>
2021-02-12 02:33:53 -0300

    Add files via upload

---
+++ Adjuster/Adjuster.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adjuster
+{
+    class Adjuster
+    {
+        
+            static int qtde;
+            static AdjusterBase adj = new AdjusterBase();
+            static int[] vetor = new int[qtde];
+            static double perc;
+
+            public static void QuantityReader()
+            {
+
+                Console.Write("How Many Employees? ");
+                qtde = int.Parse(Console.ReadLine());
+
+
+                if (qtde < 2 || qtde > 200)
+                {
+                     do
+                     {
+                        Console.WriteLine("Choose again the quantity! (Choose between 2 and 200)");
+                        Console.Write("Re-enter the quantity: ");
+                        qtde = int.Parse(Console.ReadLine());
+                     } while (qtde < 2 || qtde > 200);
+                }
+            }
+
+            public static void ReadjustPercentage()
+            {
+                Console.WriteLine("Enter with the readjust percentage: ");
+                perc = double.Parse(Console.ReadLine());
+
+                adj.readjusterValue(perc);
+            }
+
+            public static void Entry()
+            {
+                ReadjustPercentage();
+
+                for (int i = 0; i < qtde; i++)
+                {
+                    Console.Write("Days Quantity: ");
+                    adj.qtdDays = int.Parse(Console.ReadLine());
+
+                    Console.Write("Hour Value: ");
+                    adj.valueHour = double.Parse(Console.ReadLine());
+
+                    Exit();
+                }
+            }
+
+            public static void Exit()
+            {               
+                Console.WriteLine("Hour Value: {0:c}", adj.valueHour);
+                Console.WriteLine("Days Quantity: {0}", adj.qtdDays);
+                Console.WriteLine("Wage: {0:c}", adj.salCalc);
+            }
+
+            public static void Main(string[] args)
+            {
+                QuantityReader();
+                Entry();
+
+                Console.ReadKey();
+            }
+    }
+}
---
+++ Adjuster/AdjusterBase.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adjuster
+{
+    class AdjusterBase
+    {
+
+        private double _valueHour;
+        private int _qtdDays;
+
+        public double valueHour
+        {
+            set
+            {
+                if (value > 220)
+                {
+                    _valueHour = 220;
+                }
+                else if (value < 40)
+                    _valueHour = 40;
+                else
+                    _valueHour = value;
+            }
+            get
+            {
+                return _valueHour;
+            }
+        }
+
+        public int qtdDays
+        {
+            set
+            {
+                if (value > 40)
+                {
+                    _qtdDays = 40;
+                }
+                else if (value < 16)
+                {
+                    _qtdDays = 16;
+                }
+                else
+                    _qtdDays = value;
+            }
+            get
+            {
+                return _qtdDays;
+            }
+        }
+        public double salCalc
+        {
+            get
+            {
+                return qtdDays * valueHour * 2.5f;
+            }
+        }
+
+
+        public void readjusterValue(double x)
+        {
+           valueHour += valueHour * x / 100;
+        }
+
+        public AdjusterBase()
+        {
+           qtdDays = 16;
+           valueHour = 40;
+        }
+        public AdjusterBase(int qtdD, double vlH)
+        {
+           qtdDays = qtdD;
+           valueHour = vlH;
+        }
+
+    }
+}
